9c741047c665758b3c9307f46238553fdb304d1f
krishmanicks <krishanthmanicks@gmail.com>
2021-06-30 10:12:28 +0530

    Updated_cODE

---
+++ LowestAncestor.java
@@ -0,0 +1,49 @@
+class Node
+{
+  int data;
+  Node left;
+  Node right;
+  public Node(int key)
+  {
+    this.data = key;
+    left = right = null;
+  }
+}
+
+
+class LowestAncestor
+{
+  public Node root;
+
+Node LCA(Node node, int n1, int n2)
+{
+  while(node != null)
+  {
+    if(node.data > n1 & node.data > n2)
+        node = node.left;
+
+    else if(node.data < n1 & node.data < n2)
+        node = node.right;
+
+    else
+        break;
+  }
+  return node;
+}
+
+  public static void main(String[] args) {
+    LowestAncestor low = new LowestAncestor();
+
+    low.root = new Node(20);
+    low.root.left = new Node(8);
+    low.root.left.left = new Node(4);
+    low.root.right = new Node(22);
+    low.root.left.right = new Node(12);
+    low.root.left.right.left = new Node(10);
+    low.root.left.right.right = new Node(14);
+
+    int n1 = 10, n2 = 4;
+    System.out.println(low.LCA(low.root, n1, n2).data);
+
+  }
+}
