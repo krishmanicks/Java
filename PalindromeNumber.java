c0fc7daa902b0e2e7d6725c1ebaa7cd3cdf5748e
manicks <krishanth.sm@zohocorp.com>
2025-12-15 20:42:30 +0530

    palindrome number

---
+++ PalindromeNumber.java
@@ -0,0 +1,27 @@
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int y = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x = x / 10;
+        }
+
+        if (y == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        boolean s = palindromeNumber.isPalindrome(121);
+        System.out.println(s);
+    }
+}
